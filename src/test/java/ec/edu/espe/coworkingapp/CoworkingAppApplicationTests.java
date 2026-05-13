314bd25c3a323db2f52882cf9052767b3cb3fe64
RafaelVieiraV <jrvieira@espe.edu.ec>
2026-05-13 16:53:45 -0500

    fix: configurar test con H2 y WebEnvironment.NONE

--- src/test/java/ec/edu/espe/coworkingapp/CoworkingAppApplicationTests.java
+++ src/test/java/ec/edu/espe/coworkingapp/CoworkingAppApplicationTests.java
@@ -2,8 +2,9 @@ package ec.edu.espe.coworkingapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.NONE)
 class CoworkingAppApplicationTests {
 
     @Test
