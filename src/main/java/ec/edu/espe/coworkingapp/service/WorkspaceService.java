14ec78fcd7166129704158a2f4cce85d6206919d
ronaldpg <rjpuruncajas@espe.edu.ec>
2026-05-16 18:57:22 -0500

    Metodos CRUD y busqueda para los workspace

--- src/main/java/ec/edu/espe/coworkingapp/service/WorkspaceService.java
+++ src/main/java/ec/edu/espe/coworkingapp/service/WorkspaceService.java
@@ -3,6 +3,8 @@ package ec.edu.espe.coworkingapp.service;
 import ec.edu.espe.coworkingapp.domain.WorkspaceType;
 import ec.edu.espe.coworkingapp.dto.request.WorkspaceRequestDto;
 import ec.edu.espe.coworkingapp.dto.response.WorkspaceResponseDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 public interface WorkspaceService {
@@ -10,7 +12,9 @@ public interface WorkspaceService {
     WorkspaceResponseDto findById(Long id);
     List<WorkspaceResponseDto> findAll();
     List<WorkspaceResponseDto> findAllAvailable();
+    Page<WorkspaceResponseDto> searchPage(String name, Pageable pageable);
     WorkspaceResponseDto update(Long id, WorkspaceRequestDto dto);
+    void delete(Long id);
     void disable(Long id);
     List<WorkspaceResponseDto> findByType(WorkspaceType type);
 }
