5bf1b63fcf5317b1dddbb29f8363e5e132a20e25
ronaldpg <rjpuruncajas@espe.edu.ec>
2026-05-16 18:59:36 -0500

    Agregado findAll and findById paginated en reservas

--- src/main/java/ec/edu/espe/coworkingapp/repository/BookingRepository.java
+++ src/main/java/ec/edu/espe/coworkingapp/repository/BookingRepository.java
@@ -1,19 +1,23 @@
-package ec.edu.espe.coworkingapp.repository;
-
-import ec.edu.espe.coworkingapp.domain.Booking;
-import ec.edu.espe.coworkingapp.domain.BookingStatus;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import java.time.LocalDateTime;
-import java.util.List;
-
-@Repository
-public interface BookingRepository extends JpaRepository<Booking, Long> {
-    List<Booking> findByMemberId(Long memberId);
-    List<Booking> findByWorkspaceId(Long workspaceId);
-    List<Booking> findByStatus(BookingStatus status);
-    List<Booking> findByWorkspaceIdAndStatus(Long workspaceId, BookingStatus status);
-    List<Booking> findByMemberIdAndStatusNot(Long memberId, BookingStatus status);
-    List<Booking> findByWorkspaceIdAndStatusIn(Long workspaceId, List<BookingStatus> statuses);
-    List<Booking> findByMemberIdAndStatusNotAndStartDatetimeBetween(Long memberId, BookingStatus status, LocalDateTime start, LocalDateTime end);
-}
+package ec.edu.espe.coworkingapp.repository;
+
+import ec.edu.espe.coworkingapp.domain.Booking;
+import ec.edu.espe.coworkingapp.domain.BookingStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.time.LocalDateTime;
+import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+@Repository
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+    List<Booking> findByMemberId(Long memberId);
+    List<Booking> findByWorkspaceId(Long workspaceId);
+    List<Booking> findByStatus(BookingStatus status);
+    List<Booking> findByWorkspaceIdAndStatus(Long workspaceId, BookingStatus status);
+    List<Booking> findByMemberIdAndStatusNot(Long memberId, BookingStatus status);
+    List<Booking> findByWorkspaceIdAndStatusIn(Long workspaceId, List<BookingStatus> statuses);
+    List<Booking> findByMemberIdAndStatusNotAndStartDatetimeBetween(Long memberId, BookingStatus status, LocalDateTime start, LocalDateTime end);
+    Page<Booking> findById(Long id, Pageable pageable);
+    Page<Booking> findAll(Pageable pageable);
+}
