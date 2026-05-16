2d744ac47b61a9c86e34ab5e2bfcd3c2125eb14b
ronaldpg <rjpuruncajas@espe.edu.ec>
2026-05-16 18:50:31 -0500

    fixeado de la clase main de la aplicacion

---
+++ src/main/java/ec/edu/espe/coworkingapp/U1ProyectoGrupoBApplication.java
@@ -0,0 +1,9 @@
+package ec.edu.espe.coworkingapp;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class U1ProyectoGrupoBApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(U1ProyectoGrupoBApplication.class, args);
+    }
+}
