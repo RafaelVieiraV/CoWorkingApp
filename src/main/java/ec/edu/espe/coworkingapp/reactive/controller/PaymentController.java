59ec863d384e3dd56582ef6499c813e6cf0ad9c0
ronaldpg <rjpuruncajas@espe.edu.ec>
2026-06-21 22:33:02 -0500

    Actualando el controllador de pagos para que verficar si existen reservas

--- src/main/java/ec/edu/espe/coworkingapp/reactive/controller/PaymentController.java
+++ src/main/java/ec/edu/espe/coworkingapp/reactive/controller/PaymentController.java
@@ -2,6 +2,7 @@ package ec.edu.espe.coworkingapp.reactive.controller;
 
 import ec.edu.espe.coworkingapp.reactive.model.PaymentView;
 import ec.edu.espe.coworkingapp.reactive.service.PaymentService;
+import ec.edu.espe.coworkingapp.reactive.service.BookingPipelineStreamService;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
@@ -12,44 +13,48 @@ import reactor.core.publisher.Mono;
 public class PaymentController {
 
     private final PaymentService paymentService;
+    private final BookingPipelineStreamService pipelineStreamService;
 
-    public PaymentController(PaymentService paymentService) {
+    public PaymentController(PaymentService paymentService,
+                             BookingPipelineStreamService pipelineStreamService) {
         this.paymentService = paymentService;
+        this.pipelineStreamService = pipelineStreamService;
     }
 
-    // PASO 1 y 3: listar todas las reservas (devuelve [] si no hay)
     @GetMapping
     public Flux<PaymentView> getAll() {
         return paymentService.getAll();
     }
 
-    // Reservas activas (confirmadas) con su estado de pago
     @GetMapping("/active")
     public Flux<PaymentView> getActive() {
         return paymentService.getActive();
     }
 
-    // PASO 2: registrar el pago de una reserva (POST)
     @PostMapping("/{bookingId}/pay")
     public Mono<PaymentView> pay(@PathVariable Long bookingId) {
         return paymentService.pay(bookingId);
     }
 
-    // Cancelar la reserva (y su pago)
     @PostMapping("/{bookingId}/cancel")
     public Mono<PaymentView> cancel(@PathVariable Long bookingId) {
         return paymentService.cancel(bookingId);
     }
 
-    // PASO 4 y 5: promedio asíncrono (~3s) y NO bloqueante (delayElement)
     @GetMapping("/average")
     public Mono<Double> average() {
         return paymentService.averageAmount();
     }
 
-    // PASO 6: stream en tiempo real con SSE
     @GetMapping(value = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<PaymentView> stream() {
         return paymentService.stream();
+    }
+
+
+     //SSE: emite los eventos del pipeline de verificación de reservas en tiempo real
+    @GetMapping(value = "/pipeline-stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> pipelineStream() {
+        return pipelineStreamService.ejecutarYEmitirEventos();
     }
 }
